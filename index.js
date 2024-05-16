f59b987afee12419c7e2f32c7c1831cecd34dd74
Georgeccy <95911803+Georgeccy@users.noreply.github.com>
2024-05-16 10:59:49 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,96 +1,73 @@
-let homeScoresEl = document.getElementById("home-scores");
-let guestScoresEl = document.getElementById("guest-scores")
-let panelsEl = document.getElementById("panels")
-
-function plus1(count) {
-    return count + 1;
+const homeScoresEl = document.getElementById("home-scores");
+const guestScoresEl = document.getElementById("guest-scores");
+const panelsEl = document.getElementById("panels");
+const messageEl = document.getElementById("message");
+let scores = {
+    "homeScore": 0,
+    "guestScore": 0,
 }
 
-function plus2(count) {
-    return count + 2;
-}
+// To update the score according to which button the user presses.
+function updateScore(team, points){
+    messageEl.textContent = `The ${team} team scores ${points}`;
+    setTimeout(() => {
+    messageEl.textContent = "Press any "+" button to continue~"
+    }, 3000);
+    newScore = scores[`${team}Score`] += points;
+    team === 'home' ? homeScoresEl.textContent = newScore : guestScoresEl.textContent = newScore;
+};
 
-function plus3(count) {
-    return count + 3;
-}
-
+// To compare the current scores of the home team and the guest team.
 function compare() {
-    let homeScore = Number(homeScoresEl.textContent);
-    let guestScore = Number(guestScoresEl.textContent);
+    const homeScore = scores["homeScore"];
+    const guestScore = scores["guestScore"];
     return  homeScore > guestScore? 1 : homeScore < guestScore? 2 : homeScore !== 0? 3: 4;
+};
+
+// To change the color value of a certain variable
+function changeColor(variable, color) {
+    panelsEl.style.setProperty(variable, color)
 }
 
-function leader() {
+// To adjust the color of the home or guest heading, which indicates which team takes the lead.
+function whoIsLeader() {
     switch(compare()){
         case 1:
-        panelsEl.style.setProperty("--home-color", "yellow"); 
-        panelsEl.style.setProperty("--guest-color", "grey");                 
+        changeColor("--home-color", "yellow"); 
+        changeColor("--guest-color", "grey");           
         break;
         case 2: 
-        panelsEl.style.setProperty("--guest-color", "yellow");
-        panelsEl.style.setProperty("--home-color", "grey");         
+        changeColor("--guest-color", "yellow");
+        changeColor("--home-color", "grey");         
         break;    
         case 3:
-        panelsEl.style.setProperty("--home-color", "grey"); 
-        panelsEl.style.setProperty("--guest-color", "grey");         
+        changeColor("--home-color", "red"); 
+        changeColor("--guest-color", "red");         
         break;       
         case 4:
-        panelsEl.style.setProperty("--home-color", "white"); 
-        panelsEl.style.setProperty("--guest-color", "white");         
+        changeColor("--home-color", "white"); 
+        changeColor("--guest-color", "white");         
         break;  
         default:
         break;
-    }
-    }
+    };
+};
 
-function homePlus1() {
-    console.log("The home team scores 1 !")
-    let iniNum = Number(homeScoresEl.textContent);
-    homeScoresEl.textContent = plus1(iniNum);
-    leader();
-}
-function homePlus2() {
-    console.log("The home team scores 2 !")
-    let iniNum = Number(homeScoresEl.textContent);
-    homeScoresEl.textContent = plus2(iniNum);
-    leader();    
-}
-function homePlus3() {
-    console.log("The home team scores 3 !")
-    let iniNum = Number(homeScoresEl.textContent);
-    homeScoresEl.textContent = plus3(iniNum);
-    leader();
-    
-}
+// To update the score and the heading color of each team.
+function update(team, points) {
+    updateScore(team, points);
+    whoIsLeader();
+};
 
-function guestPlus1() {
-    console.log("The guest team scores 1 !")
-    let iniNum = Number(guestScoresEl.textContent);
-    guestScoresEl.textContent = plus1(iniNum);
-    leader();
-    
-}
-function guestPlus2() {
-    console.log("The guest team scores 2 !")
-    let iniNum = Number(guestScoresEl.textContent);
-    guestScoresEl.textContent = plus2(iniNum);
-    leader();
-    
-}
-function guestPlus3() {
-    console.log("The guest team scores 3 !")
-    let iniNum = Number(guestScoresEl.textContent);
-    guestScoresEl.textContent = plus3(iniNum);
-    leader();
-    
-}
 
+// To refresh the values of all relevant values to start a new game.
 function newGame() {
-    console.log("They've started a new game!");
+    messageEl.textContent = "They've started a new game!"
+    setTimeout(() => {
+    messageEl.textContent = "Press any '+' button to start!"
+    }, 3000);
+    scores["homeScore"] = scores["guestScore"] = 0;
     homeScoresEl.textContent = 0;
     guestScoresEl.textContent = 0;
-    leader();
-    
-}
-
-
+    whoIsLeader();
+};
---
+++ readme.md
@@ -0,0 +1,47 @@
+# [Basketball Scoreboard](https://scrimba.com/learn/frontend/solo-project-pro-basketball-scoreboard-cz9adVfP)
+
+Requirements:
+
+- Build it from scratch
+- Follow the design
+- Make all 6 buttons work
+
+Stretch Goals:
+
+- Add “New Game” button
+- Highlight the leader
+- Add other counters (fouls, periods, timer, etc.)
+
+Basic JavaScript concepts taught
+
+[x] Declaring and initializing variables with let
+[x] Variable reassignment
+[x] Data types: number, string
+[x] CamelCase convention for variable names
+[x] Basic math operations
+[x] Logging to the console
+[x] getElementById()
+[x] Basic exposure to writing a function *(no parameters or arguments yet)*
+[x] Calling/invoking a function
+[x] Inline onclick() event listener (students haven’t yet learned about addEventListener())
+[x] Basic intro to the DOM
+[x] .textContent 
+
+## What youve done well
+- met all core and stretch goals. Great job!
+- applied core concepts taught in the course.
+- good understanding of classes e.g. <div class="btn-group">
+- used custom font
+- understand how to use flex and its various properties
+- margin: 0 auto to center block level elements
+
+## What could be improved
+- avoid using ID selectors to style elements in CSS. Find out more about specificity in module 6, or read this article: https://www.w3schools.com/css/css_specificity.asp
+- don't use underscores in variable names. use hyphens -
+- use hover effects to give user feedback on interactive elements
+
+## Your question: how to use CSS variables
+ * declare them in :root {}
+ * use variable names that describe their **function** , not their value e.g. --primaryColor, --accent etc.
+ 
+## Challenge: refactoring to make the code DRYer with parameters and arguments
